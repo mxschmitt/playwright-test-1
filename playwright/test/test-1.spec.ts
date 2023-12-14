30f9c94ed4301b0cc85eaab734da7cbacfd3a0cc
Max Schmitt <max@schmitt.mx>
2023-12-14 11:00:34 -0800

    make it work

--- playwright/test/test-1.spec.ts
+++ playwright/test/test-1.spec.ts
@@ -4,13 +4,6 @@ import { titleVerify } from "helper";
 test("test-1@", async ({ page }) => {
     await page.goto("https://playwright.dev/");
 
-    await test.step(`Step #1: Validate Playwright Header`, async () => {
-        await expect(
-            page.locator("header h1"),
-            "Playwright header is not visible"
-        ).toBeVisible();
-    });
-
     await test.step(`Step #2: Validate Playwright Highlighted Title`, async () => {
         await titleVerify(page.locator("header h1 span"), "Playwright");
     });
